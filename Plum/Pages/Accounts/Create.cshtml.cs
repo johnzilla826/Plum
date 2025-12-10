963efce3215586f15f98d9e947fcdb613a38dddd
johnzilla826 <229241950+johnzilla826@users.noreply.github.com>
2025-12-10 08:20:27 -0600

    Create account functionality

---
+++ Plum/Pages/Accounts/Create.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Plum.Data;
+using Plum.Models;
+
+namespace Plum.Pages.Accounts;
+
+public class Create(PlumDbContext context) : PageModel
+{   
+
+    [BindProperty]
+    public Account Account { get; set;}
+    
+    public void OnGet()
+    {
+    }
+    
+    public IActionResult OnPost()
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+        
+        context.Accounts.Add(Account);
+        context.SaveChanges();
+        return RedirectToPage("./Index");
+    }
+}
---
+++ Plum/Pages/Accounts/Index.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Plum.Pages.Accounts;
+
+public class Index : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
